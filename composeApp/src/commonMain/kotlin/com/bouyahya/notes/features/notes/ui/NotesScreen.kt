94c4ec0c0fd7068c123ae4ce5a1043c68a837d5e
Bilel_Bouyahyaa <bilel.bouyahya@outlook.com>
2024-02-11 10:04:57 +0100

    show basic notes list and handle state

--- composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/NotesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/NotesScreen.kt
@@ -1,7 +1,11 @@
 package com.bouyahya.notes.features.notes.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -22,7 +26,22 @@ class NotesScreen : Screen {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "Notes Screen")
+            if (state.isLoading) {
+                CircularProgressIndicator()
+            } else if (state.error.isNotEmpty()) {
+                Text(text = state.error)
+            } else if (state.noteList.isEmpty()) {
+                Text(text = "Notes Screen")
+            } else {
+                LazyColumn(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(state.noteList) {
+                        Text(text = it.title)
+                    }
+                }
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/NotesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/NotesViewModel.kt
@@ -4,6 +4,7 @@ import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import com.bouyahya.notes.features.notes.domain.NoteRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class NotesViewModel(
@@ -12,13 +13,34 @@ class NotesViewModel(
 ) : ScreenModel {
     init {
         screenModelScope.launch {
+            state.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
             noteRepository
                 .getAllNotes()
-                .onSuccess {
-                    println("noteRepositoryOnSuccess $it")
+                .onSuccess { notes ->
+                    println("noteRepositoryOnSuccess $notes")
+                    state.update {
+                        it.copy(
+                            noteList = notes
+                        )
+                    }
                 }.onFailure {
                     println("noteRepositoryOnFailure $it")
+                    state.update {
+                        it.copy(
+                            error = "Something went wrong!"
+                        )
+                    }
                 }
+        }.invokeOnCompletion {
+            state.update {
+                it.copy(
+                    isLoading = false
+                )
+            }
         }
     }
 }
