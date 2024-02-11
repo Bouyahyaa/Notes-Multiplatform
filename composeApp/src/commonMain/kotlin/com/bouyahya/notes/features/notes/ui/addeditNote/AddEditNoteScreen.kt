519846f086917a9fbcf61e3b1d49e20eb6d0134a
Bilel_Bouyahyaa <bilel.bouyahya@outlook.com>
2024-02-11 20:12:57 +0100

    handle error scenario when add note

--- composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/addeditNote/AddEditNoteScreen.kt
+++ composeApp/src/commonMain/kotlin/com/bouyahya/notes/features/notes/ui/addeditNote/AddEditNoteScreen.kt
@@ -30,6 +30,7 @@ class AddEditNoteScreen : Screen {
         val viewModel = getScreenModel<AddEditNoteViewModel>()
         val state by viewModel.state.collectAsState()
         val note = state.note.value
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
 
         val focusManager = LocalFocusManager.current
         val keyboardController = LocalSoftwareKeyboardController.current
@@ -40,104 +41,112 @@ class AddEditNoteScreen : Screen {
                 if (state == ValidationEvent.Success) {
                     navigator.pop()
                 } else {
-                    println("Error")
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = "Something went wrong!",
+                        actionLabel = "Dismiss"
+                    )
                 }
             }
         }
 
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(15.dp)
-                .clickable(
-                    interactionSource = MutableInteractionSource(),
-                    indication = null,
-                ) {
-                    focusManager.clearFocus()
-                    keyboardController?.hide()
-                }
+        Scaffold(
+            scaffoldState = scaffoldState,
+            backgroundColor = Color.Transparent
         ) {
-            // Return icon button
-            IconButton(onClick = {
-                navigator.pop()
-            }) {
-                Icon(
-                    Icons.Default.ArrowBack,
-                    contentDescription = "Return"
-                )
-            }
-
-            // Title TextField
-            OutlinedTextField(
-                value = note.title,
-                onValueChange = {
-                    viewModel.onEvent(
-                        AddEditNoteEvent.UpdateNoteFields(
-                            note.copy(
-                                title = it
-                            )
-                        )
-                    )
-                },
-                label = { Text("Title") },
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
-                )
-            )
-
-            // Description TextField
-            OutlinedTextField(
-                value = note.description,
-                onValueChange = {
-                    viewModel.onEvent(
-                        AddEditNoteEvent.UpdateNoteFields(
-                            note.copy(
-                                description = it
-                            )
-                        )
-                    )
-                },
-                label = { Text("Description") },
-                minLines = 10,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        keyboardController?.hide()
+                    .fillMaxSize()
+                    .padding(15.dp)
+                    .clickable(
+                        interactionSource = MutableInteractionSource(),
+                        indication = null,
+                    ) {
                         focusManager.clearFocus()
+                        keyboardController?.hide()
                     }
-                )
-            )
-
-            // Submit Button
-            Button(
-                onClick = {
-                    if (!state.isLoading)
-                        viewModel.onEvent(AddEditNoteEvent.Submit)
-                },
-                enabled = !state.isLoading,
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = MaterialTheme.colors.secondaryVariant
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
             ) {
-                Text(
-                    text = "Add note",
-                    color = Color.White
+                // Return icon button
+                IconButton(onClick = {
+                    navigator.pop()
+                }) {
+                    Icon(
+                        Icons.Default.ArrowBack,
+                        contentDescription = "Return"
+                    )
+                }
+
+                // Title TextField
+                OutlinedTextField(
+                    value = note.title,
+                    onValueChange = {
+                        viewModel.onEvent(
+                            AddEditNoteEvent.UpdateNoteFields(
+                                note.copy(
+                                    title = it
+                                )
+                            )
+                        )
+                    },
+                    label = { Text("Title") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    )
                 )
+
+                // Description TextField
+                OutlinedTextField(
+                    value = note.description,
+                    onValueChange = {
+                        viewModel.onEvent(
+                            AddEditNoteEvent.UpdateNoteFields(
+                                note.copy(
+                                    description = it
+                                )
+                            )
+                        )
+                    },
+                    label = { Text("Description") },
+                    minLines = 10,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                            focusManager.clearFocus()
+                        }
+                    )
+                )
+
+                // Submit Button
+                Button(
+                    onClick = {
+                        if (!state.isLoading)
+                            viewModel.onEvent(AddEditNoteEvent.Submit)
+                    },
+                    enabled = !state.isLoading,
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = MaterialTheme.colors.secondaryVariant
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = "Add note",
+                        color = Color.White
+                    )
+                }
             }
         }
     }
